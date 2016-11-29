cb3f362522faeff881cedf3d33f811b89d740579
csg <csg@dhcp-7-9-249.univ-tlse1.fr>
2016-11-29 11:32:38 +0100

    adding unimplemented methods

--- spin/src/main/java/spin/algo/generator/SFGenerator.java
+++ spin/src/main/java/spin/algo/generator/SFGenerator.java
@@ -1,5 +1,14 @@
 package spin.algo.generator;
 
+import core.metamodel.IPopulation;
+import spin.objects.SpinNetwork;
+
 public class SFGenerator implements INetworkGenerator {
+
+	@Override
+	public SpinNetwork generateNetwork(IPopulation population) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
