81b22ea605859f3cac87aef35112b0257e07bf09
Felix <felix@felix.net>
2016-11-29 11:25:36 +0100

    Spec interface plus précise.

--- spin/src/main/java/spin/algo/generator/INetworkGenerator.java
+++ spin/src/main/java/spin/algo/generator/INetworkGenerator.java
@@ -1,13 +1,25 @@
 package spin.algo.generator;
 
 import spin.objects.NetworkLink;
+import spin.objects.SpinNetwork;
+import core.io.survey.attribut.ASurveyAttribute;
+import core.io.survey.attribut.value.AValue;
+import core.metamodel.IEntity;
+import core.metamodel.IPopulation;
 
 /** Interface de générateur de réseau, commun a tous les générateurs.
  * 
  *
  */
 public interface INetworkGenerator <T extends NetworkLink, NetworkNode>{
-
+	/** Methode de création d'un réseau prenant une population générique et créant
+	 * le réseau associé. 
+	 * 
+	 * @param population population en paramètre, implementant l'interface IPopulation 
+	 * @return un SpinNetwork
+	 */
+	public SpinNetwork generateNetwork(IPopulation<IEntity<ASurveyAttribute, AValue>, ASurveyAttribute, AValue> population);
+	
 	
 	
 	
--- spin/src/main/java/spin/algo/generator/SWGenerator.java
+++ spin/src/main/java/spin/algo/generator/SWGenerator.java
@@ -1,6 +1,15 @@
 package spin.algo.generator;
 
+import spin.objects.SpinNetwork;
+import core.metamodel.IPopulation;
+
 public class SWGenerator implements INetworkGenerator {
+
+	@Override
+	public SpinNetwork generateNetwork(IPopulation population) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 	
 	
 	
