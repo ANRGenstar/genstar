9385e40c877fca5f1b1231651ec9979b22b2a53c
benoitgaudou <benoit.gaudou@gmail.com>
2016-12-01 13:09:09 +0100

    SPIN: ell test

---
+++ spin/test/spin/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({RandomNetworkGeneratorTest.class,SpinNetworkTest.class})
+public class AllTests {
+
+}
