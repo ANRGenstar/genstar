f055b06374d402336e309f9e6cba1e4d4e35657e
Samuel Thiriot <samuel.thiriot.accounts@res-ear.ch>
2017-06-27 10:35:58 +0200

    accept null attributes in a population

--- gospl/src/main/java/gospl/GosplPopulation.java
+++ gospl/src/main/java/gospl/GosplPopulation.java
@@ -14,6 +14,7 @@ import core.metamodel.pop.APopulationValue;
 public class GosplPopulation implements IPopulation<APopulationEntity, APopulationAttribute, APopulationValue> {
 	
 	private final Collection<APopulationEntity> population;
+	private Collection<APopulationAttribute> attributes = null;
 	
 	/**
 	 * Default inner type collection is {@link Set}
@@ -38,6 +39,35 @@ public class GosplPopulation implements IPopulation<APopulationEntity, APopulati
 			this.population = population;
 	}
 	
+	/**
+	 * throws an exception if this entity does not has the reference attributes
+	 * @param e
+	 */
+	protected final void _checkEntityAttributes(APopulationEntity e) throws IllegalArgumentException {
+		if ((this.attributes != null) && (!e.getAttributes().equals(this.attributes)))
+			throw new IllegalArgumentException(
+					"the entity should contain attributes "+
+					attributes.toString()
+			);
+			
+	}
+	
+	/** 
+	 * defines the attributes expected to be present for all the entities
+	 * in this population. Further calls to add() will raise exceptions if these attributes
+	 * are not defined for the novel entity.
+	 * 
+	 * @param attributes
+	 */
+	public void setExpectedAttributes(Collection<APopulationAttribute> attributes) {
+		this.attributes = attributes;
+		
+		// check past entities
+		for (APopulationEntity e: population) {
+			_checkEntityAttributes(e);
+		}
+	}
+
 	@Override
 	public int size() {
 		return population.size();
@@ -70,6 +100,8 @@ public class GosplPopulation implements IPopulation<APopulationEntity, APopulati
 
 	@Override
 	public boolean add(APopulationEntity e) {
+		if (attributes != null)
+			_checkEntityAttributes(e);
 		return population.add(e);
 	}
 
@@ -106,7 +138,11 @@ public class GosplPopulation implements IPopulation<APopulationEntity, APopulati
 // ------------------------------------ POP ACCESSORS ------------------------------------ //
 	
 	public Set<APopulationAttribute> getPopulationAttributes(){
-		return population.parallelStream().flatMap(e -> e.getAttributes().stream()).collect(Collectors.toSet());
+		if (attributes == null)
+			// rebuild the list of attributes
+			return population.parallelStream().flatMap(e -> e.getAttributes().stream()).collect(Collectors.toSet());
+		else 
+			return new HashSet<>(attributes);
 	}
 	
 }
