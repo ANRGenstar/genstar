cd081cfec8ceee123c3041668fe65e6b768d86cb
chapuisk <kevin.chapuis@gmail.com>
2017-10-31 11:20:35 +0700

    dictionary init

--- core/src/main/java/core/configuration/dictionary/DemographicDictionary.java
+++ core/src/main/java/core/configuration/dictionary/DemographicDictionary.java
@@ -1,5 +1,7 @@
 package core.configuration.dictionary;
 
+import java.util.Arrays;
+import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -23,20 +25,49 @@ public class DemographicDictionary {
 
 	private Set<DemographicAttribute<? extends IValue>> attSet;
 	
-	private Set<AgregatedDemographicAttribute<? extends IValue>> agregAttSet;
+	private Set<AgregatedDemographicAttribute<? extends IValue>> aggregAttSet;
 	private Set<MappedDemographicAttribute<? extends IValue, IValue>> mappedAttSet;
 	
 	private Set<MappedDemographicAttribute<? extends IValue, IValue>> recordAttSet;
 	
+	public DemographicDictionary() {
+		attSet = new HashSet<>();
+	}
 	
 	public Set<DemographicAttribute<? extends IValue>> getAttributes() {
 		return Stream.concat(mappedAttSet.stream(), 
-				Stream.concat(attSet.stream(), agregAttSet.stream()))
+				Stream.concat(attSet.stream(), aggregAttSet.stream()))
 				.collect(Collectors.toSet());
 	}
 	
 	public Set<MappedDemographicAttribute<? extends IValue, IValue>> getRecordAttribute(){
 		return recordAttSet;
 	}
+	
+	// ---------------------------- SETTERS ---------------------------- //
+	
+	@SuppressWarnings("unchecked")
+	public DemographicDictionary addAttributes(DemographicAttribute<? extends IValue>... attributes) {
+		this.attSet.addAll(Arrays.asList(attributes));
+		return this;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public DemographicDictionary addAggregatedAttributes(AgregatedDemographicAttribute<? extends IValue>... attributes) {
+		this.aggregAttSet.addAll(Arrays.asList(attributes));
+		return this;
+	}
 
+	@SuppressWarnings("unchecked")
+	public DemographicDictionary addMappedAttributes(MappedDemographicAttribute<? extends IValue, IValue>... attributes) {
+		this.mappedAttSet.addAll(Arrays.asList(attributes));
+		return this;
+	}
+	
+	@SuppressWarnings("unchecked")
+	public DemographicDictionary addRecordAttributes(MappedDemographicAttribute<? extends IValue, IValue>... attributes) {
+		this.recordAttSet.addAll(Arrays.asList(attributes));
+		return this;
+	}
+	
 }
