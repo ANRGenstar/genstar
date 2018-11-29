e9abc5dc4f218fa5e6a2071de60f31d593f1919e
rey-coyrehourcq <sebastien.rey-coyrehourcq@univ-rouen.fr>
2018-11-29 10:06:18 +0100

    add travis build number as argument for deploy

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -ev
 
-mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=1 clean package javadoc:jar source:jar deploy
+mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar deploy
 
