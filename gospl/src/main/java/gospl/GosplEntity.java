4cb4df265ba75e3dfdbda359fdfd93f78e9da04a
chapuisk <kevin.chapuis@gmail.com>
2018-12-09 11:54:11 +0700

    roll back

--- gospl/src/main/java/gospl/GosplEntity.java
+++ gospl/src/main/java/gospl/GosplEntity.java
@@ -1,16 +1,11 @@
 package gospl;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
 import core.metamodel.attribute.Attribute;
-import core.metamodel.attribute.EmergentAttribute;
-import core.metamodel.attribute.IAttribute;
 import core.metamodel.entity.ADemoEntity;
-import core.metamodel.entity.IEntity;
 import core.metamodel.value.IValue;
-import core.metamodel.value.numeric.IntegerValue;
 
 /**
  * A GoSPL Entity is a population entity
@@ -20,16 +15,8 @@ import core.metamodel.value.numeric.IntegerValue;
  */
 public class GosplEntity extends ADemoEntity {
 	
-	private EmergentAttribute<IntegerValue, Collection<IEntity<? extends IAttribute<? extends IValue>>>, ?> sizeAttribute;
-	
 	public GosplEntity(Map<Attribute<? extends IValue>, IValue> attributes){
 		super(attributes);
-	}
-	
-	public GosplEntity(Map<Attribute<? extends IValue>, IValue> attributes, 
-			EmergentAttribute<IntegerValue, Collection<IEntity<? extends IAttribute<? extends IValue>>>, ?> sizeAttribute) {
-		super(attributes);
-		this.sizeAttribute = sizeAttribute;
 	}
 
 	public GosplEntity(){
@@ -39,11 +26,6 @@ public class GosplEntity extends ADemoEntity {
 	@Override
 	public GosplEntity clone(){
 		return new GosplEntity(new HashMap<>(this.getAttributeMap()));
-	}
-
-	@Override
-	public IntegerValue getCountChildren() {
-		return sizeAttribute.getEmergentValue(this);
 	}
 
 
