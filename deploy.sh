cb0e28eb56215977c6bcc5347d03f1627675a39b
benoitgaudou <benoit.gaudou@gmail.com>
2019-08-14 10:02:31 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ev
 
-mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar deploy
+mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar compile
+#mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar deploy
 
