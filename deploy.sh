4f4e12be33f7492588e3ca655f682d782368e2ac
benoitgaudou <benoit.gaudou@gmail.com>
2019-08-18 12:59:44 +0700

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -ev
 
-mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar compile
+
+#mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar compile
+
 #mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean package javadoc:jar source:jar deploy
+mvn -Drevision=1.0.3.$TRAVIS_BUILD_NUMBER-SNAPSHOT -Dbuildnumber=$TRAVIS_BUILD_NUMBER clean org.jacoco:jacoco-maven-plugin:prepare-agent package javadoc:jar source:jar compile install sonar:sonar -Dsonar.projectKey=ANRGenstar_genstar
 
